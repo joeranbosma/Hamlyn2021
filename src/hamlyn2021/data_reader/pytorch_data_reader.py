84af6ccef1f55150b8e5dc7b1c2e950203f6fada
Joeran <joeran@bosma.co>
2021-12-07 21:13:21 +0100

    Bugfix

--- src/hamlyn2021/data_reader/pytorch_data_reader.py
+++ src/hamlyn2021/data_reader/pytorch_data_reader.py
@@ -37,7 +37,7 @@ class CustomDatasetLoaderRandom(CustomDatasetLoader):
         if "[random_style]" in input_fn:
             # randomly choose one of the five styles
             num = np.random.randint(0, 4+1)
-            input_fn = input_fn.replace("[random_style]", f"style_{num}")
+            input_fn = input_fn.replace("[random_style]", str(num))
 
         input_path = os.path.join(self.input_dir, input_fn)
         depth_path = os.path.join(self.depth_dir, depth_fn)
@@ -54,10 +54,9 @@ def setup_dataloader(input_dir, depth_dir, folders, cases=None, batch_size=32, s
     if cases is None:
         cases = [f"{i:04d}" for i in range(2000)]
     input_files = [
-        f"{folder}/style_0{style_num}/img{case}.png"
+        f"{folder}/style_0[random_style]/img{case}.png"
         for case in cases
         for folder in folders
-        for style_num in range(5)
     ]
     depth_files = [
         f"{folder}/depths/depth{case}.exr"
